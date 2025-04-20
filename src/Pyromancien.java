258ce58f0153b98054bd0aced00007cf2dce2c09
MOREAUB3il <moreaub@3il.fr>
2025-04-20 17:51:10 +0200

    Create Pyromancien.java

---
+++ src/Pyromancien.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Pyromancien  extends Mage{
+	
+	private Pyromancien() {
+        super(nomAleatoire());
+    }
+	
+	
+	
+	private static final String[] NOMS_POSSIBLES = {
+	        "🧙‍♂️ Vaelrik Cendreflamme 🔥 ", "🧙‍♂️ Ashara la Fournaise Écarlate 🔥 ","🧙‍♂️ Drazul le Brasier Noir 🔥 ","🧙‍♂️ Kaelthar le Fils du Brasier 🔥 ","🧙‍♂️ Morgath Sombreflamme 🔥 "
+	    };
+	
+	private static String nomAleatoire() {
+        Random rand = new Random();
+        return NOMS_POSSIBLES[rand.nextInt(NOMS_POSSIBLES.length)];
+    }
+	
+	
+	public void attaque1(Monstre cible) {
+    	int degats;
+    	if (getNiveau() < 5) {
+            degats = getForce() + getMalediction() * (cible.getPv()/4);
+        } else if (getNiveau() < 15) {
+            degats = getForce() + getMalediction() * (cible.getDefence()/2);
+        } else {
+            degats = getForce() + getMalediction() * cible.getDefence();
+        }
+        System.out.println(getNom() + " 🪄 attaque " + cible.getNom() + " avec son baton !");
+        System.out.println("Cela inflige " + degats + " dégâts !");
+        
+        cible.subirDegats(degats); 
+    }
+}
