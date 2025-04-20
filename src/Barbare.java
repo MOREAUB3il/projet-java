b946dc0ad2e61607d848d38a2b5246886113102e
MOREAUB3il <moreaub@3il.fr>
2025-04-20 16:45:13 +0200

    Create Barbare.java

---
+++ src/Barbare.java
@@ -0,0 +1,35 @@
+
+import java.util.Random;
+
+public  class Barbare extends Dps {
+    
+    private static final String[] NOMS_POSSIBLES = {
+    		" 🔪 Gorvok Crâne-Sanglant 🪓 "," 🔪 Olaf le Berserker 🪓 "," 🔪 Varkh le Hurleur d’Abîme 🪓 ","🔪 Korgash le Maudit 🪓 "," 🔪 Dragan Croc-de-Fer 🪓 "
+    };
+
+    public Barbare() {
+        super(nomAleatoire());
+    }
+
+    private static String nomAleatoire() {
+        Random rand = new Random();
+        return NOMS_POSSIBLES[rand.nextInt(NOMS_POSSIBLES.length)];
+    }
+   
+    public void attaque1(Monstre cible) {
+    	int degats;
+    	if (getNiveau() < 5) {
+            degats = getForce() + getPenetration() * (cible.getDefence()/4);
+        } else if (getNiveau() < 15) {
+            degats = getForce() + getPenetration() * (cible.getDefence()/2);
+        } else {
+            degats = getForce() + getPenetration() * cible.getDefence();
+        }
+        System.out.println(getNom() + " 🗡️ attaque " + cible.getNom() + " avec sa dague !");
+        System.out.println("Cela inflige " + degats + " dégâts !");
+        
+        cible.subirDegats(degats); 
+    }
+
+
+}
